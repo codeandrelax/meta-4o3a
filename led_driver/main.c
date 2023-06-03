e3915126fc0108b7f37d21907a9aba8300254188
Cetic99 <nikolacetic8@gmail.com>
2023-06-03 14:16:38 +0200

    timer one

--- led_driver/main.c
+++ led_driver/main.c
@@ -8,7 +8,7 @@
 
 #define LED_PIN 20
 #define NUM_DEV 26
-#define NUM_LEDS NUM_DEV*3
+#define NUM_LEDS NUM_DEV * 3
 #define DRIVER_NAME "leddriver"
 #define DRIVER_CLASS "ModuleClass"
 
@@ -26,6 +26,42 @@ static dev_t my_device_nr;
 static struct class *my_class;
 static struct cdev my_device;
 
+static struct hrtimer hrtimer_one;
+static ktime_t ktime_one;
+static int state_one;
+
+enum hrtimer_restart hrtimer_one_handler(struct hrtimer *timer)
+{
+	// Timer callback function logic
+	printk(KERN_INFO "Timer callback\n");
+
+	switch (state_one)
+	{
+	case 0:
+		gpio_set_value(LED_PIN, 0);
+		hrtimer_forward_now(timer, ns_to_ktime(pTime * 1000));
+		state_one = 1;
+		return HRTIMER_RESTART;
+		break;
+
+	case 1:
+		gpio_set_value(LED_PIN, 1);
+		hrtimer_forward_now(timer, ns_to_ktime(pTime * 1000));
+		state_one = 2;
+		return HRTIMER_RESTART;
+		break;
+
+	case 2:
+		gpio_set_value(LED_PIN, 0);
+		hrtimer_forward_now(timer, ns_to_ktime(pTime * 1000 * 3));
+		state_one = 0;
+		return HRTIMER_RESTART;
+		break;
+	}
+
+	return HRTIMER_NORESTART;
+}
+
 void writeLED(char, char, char, char);
 void writeZero(void);
 void writeOne(void);
@@ -35,85 +71,99 @@ void waitEOS(void);
 void waitGSLAT(void);
 void writeData(char);
 
-void writeLED(char dev_num, char red, char green, char blue) {
-  for (int i = 0; i < NUM_DEV; i++) {
-    writeCommTimer();
-    
-    if (i == dev_num){
-      	writeCommand();
-	writeData(blue);
-	writeData(green);
-	writeData(red);
-    }
-    waitEOS();
-  }
-  waitGSLAT();
+void writeLED(char dev_num, char red, char green, char blue)
+{
+	for (int i = 0; i < NUM_DEV; i++)
+	{
+		writeCommTimer();
+
+		if (i == dev_num)
+		{
+			writeCommand();
+			writeData(blue);
+			writeData(green);
+			writeData(red);
+		}
+		waitEOS();
+	}
+	waitGSLAT();
 }
 
-void writeZero(void) {
-  gpio_set_value(LED_PIN, 1);
-  udelay(pTime);
-  //nanosleep((const struct timespec[]){{0, pTime*1000}}, NULL);
-  gpio_set_value(LED_PIN, 0);
-  udelay(pTime*5);
-  //nanosleep((const struct timespec[]){{0, pTime*5*1000}}, NULL); 
+void writeZero(void)
+{
+	gpio_set_value(LED_PIN, 1);
+	udelay(pTime);
+	// nanosleep((const struct timespec[]){{0, pTime*1000}}, NULL);
+	gpio_set_value(LED_PIN, 0);
+	udelay(pTime * 5);
+	// nanosleep((const struct timespec[]){{0, pTime*5*1000}}, NULL);
 }
 
-void writeOne(void) {
-  gpio_set_value(LED_PIN, 1);
-  udelay(pTime);
-  //nanosleep((const struct timespec[]){{0, pTime*1000}}, NULL);
-  gpio_set_value(LED_PIN, 0);
-  udelay(pTime);
-  //nanosleep((const struct timespec[]){{0, pTime*1000}}, NULL);
-  gpio_set_value(LED_PIN, 1);
-  udelay(pTime);
-  //nanosleep((const struct timespec[]){{0, pTime*1000}}, NULL);
-  gpio_set_value(LED_PIN, 0);
-  udelay(pTime*3);
-  //nanosleep((const struct timespec[]){{0, pTime*3*1000}}, NULL);
+void writeOne(void)
+{
+	gpio_set_value(LED_PIN, 1);
+	hrtimer_start(&hrtimer_one, ktime_one, HRTIMER_MODE_REL);
+	//   gpio_set_value(LED_PIN, 1);
+	//   udelay(pTime);
+	//   //nanosleep((const struct timespec[]){{0, pTime*1000}}, NULL);
+	//   gpio_set_value(LED_PIN, 0);
+	//   udelay(pTime);
+	//   //nanosleep((const struct timespec[]){{0, pTime*1000}}, NULL);
+	//   gpio_set_value(LED_PIN, 1);
+	//   udelay(pTime);
+	//   //nanosleep((const struct timespec[]){{0, pTime*1000}}, NULL);
+	//   gpio_set_value(LED_PIN, 0);
+	//   udelay(pTime*3);
+	//   //nanosleep((const struct timespec[]){{0, pTime*3*1000}}, NULL);
 }
 
-void writeCommTimer(void) {
-  writeZero();
-  writeZero();
+void writeCommTimer(void)
+{
+	writeZero();
+	writeZero();
 }
 
-void writeCommand(void) {
-  writeOne();
-  writeOne();
-  
-  writeOne();
-  writeZero();
-  writeOne();
-  writeZero();
+void writeCommand(void)
+{
+	writeOne();
+	writeOne();
+
+	writeOne();
+	writeZero();
+	writeOne();
+	writeZero();
 }
 
-//end of sequence (for a single driver chip)
-void waitEOS(void) {
-  gpio_set_value(LED_PIN, 0);
-  udelay(tCycle*4);
-  //nanosleep((const struct timespec[]){{0, tCycle*4*1000}}, NULL); // min 3.5 to max 5.5 times tCycle
+// end of sequence (for a single driver chip)
+void waitEOS(void)
+{
+	gpio_set_value(LED_PIN, 0);
+	udelay(tCycle * 4);
+	// nanosleep((const struct timespec[]){{0, tCycle*4*1000}}, NULL); // min 3.5 to max 5.5 times tCycle
 }
 
-//grayscale latch (for the end of a chain of driver chips)
-void waitGSLAT(void) {
-  gpio_set_value(LED_PIN, 0);
-  udelay(tCycle*10);
-  //nanosleep((const struct timespec[]){{0, tCycle*10*1000}}, NULL); // minimum 8 time tCycle
+// grayscale latch (for the end of a chain of driver chips)
+void waitGSLAT(void)
+{
+	gpio_set_value(LED_PIN, 0);
+	udelay(tCycle * 10);
+	// nanosleep((const struct timespec[]){{0, tCycle*10*1000}}, NULL); // minimum 8 time tCycle
 }
 
-void writeData(char data) {
-  for (char i = 0; i<8; i++) {
-    if(data & 0b10000000)
-		writeOne();
-    else
-		writeZero();
-    data <<= 1;
-  }
+void writeData(char data)
+{
+	for (char i = 0; i < 8; i++)
+	{
+		if (data & 0b10000000)
+			writeOne();
+		else
+			writeZero();
+		data <<= 1;
+	}
 }
 
-static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
+static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs)
+{
 	int to_copy, not_copied, delta;
 
 	/* Get amount of data to copy */
@@ -131,7 +181,8 @@ static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, l
 /**
  * @brief Write data to buffer
  */
-static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
+static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs)
+{
 	int to_copy, not_copied, delta, values[4];
 
 	/* Get amount of data to copy */
@@ -139,7 +190,7 @@ static ssize_t driver_write(struct file *File, const char *user_buffer, size_t c
 
 	/* Copy data to user */
 	not_copied = copy_from_user(buffer, user_buffer, to_copy);
-	
+
 	sscanf(buffer, "%d %d %d %d", &values[0], &values[1], &values[2], &values[3]);
 	writeLED(values[0], values[1], values[2], values[3]);
 
@@ -152,7 +203,8 @@ static ssize_t driver_write(struct file *File, const char *user_buffer, size_t c
 /**
  * @brief This function is called, when the device file is opened
  */
-static int driver_open(struct inode *device_file, struct file *instance) {
+static int driver_open(struct inode *device_file, struct file *instance)
+{
 	printk("dev_nr - open was called!\n");
 	return 0;
 }
@@ -160,7 +212,8 @@ static int driver_open(struct inode *device_file, struct file *instance) {
 /**
  * @brief This function is called, when the device file is opened
  */
-static int driver_close(struct inode *device_file, struct file *instance) {
+static int driver_close(struct inode *device_file, struct file *instance)
+{
 	printk("dev_nr - close was called!\n");
 	return 0;
 }
@@ -170,27 +223,30 @@ static struct file_operations fops = {
 	.open = driver_open,
 	.release = driver_close,
 	.read = driver_read,
-	.write = driver_write
-};
+	.write = driver_write};
 
-static int __init ModuleInit(void){
+static int __init ModuleInit(void)
+{
 	printk("Hello, Kernel!\n");
 
 	/* Allocate a device nr */
-	if( alloc_chrdev_region(&my_device_nr, 0, 1, DRIVER_NAME) < 0) {
+	if (alloc_chrdev_region(&my_device_nr, 0, 1, DRIVER_NAME) < 0)
+	{
 		printk("Device Nr. could not be allocated!\n");
 		return -1;
 	}
 	printk("Led driver - Device Nr. Major: %d, Minor: %d was registered!\n", my_device_nr >> 20, my_device_nr && 0xfffff);
 
 	/* Create device class */
-	if((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+	if ((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL)
+	{
 		printk("Device class can not be created!\n");
 		goto ClassError;
 	}
 
 	/* create device file */
-	if(device_create(my_class, NULL, my_device_nr, NULL, DRIVER_NAME) == NULL) {
+	if (device_create(my_class, NULL, my_device_nr, NULL, DRIVER_NAME) == NULL)
+	{
 		printk("Can not create device file!\n");
 		goto FileError;
 	}
@@ -199,22 +255,28 @@ static int __init ModuleInit(void){
 	cdev_init(&my_device, &fops);
 
 	/* Regisering device to kernel */
-	if(cdev_add(&my_device, my_device_nr, 1) == -1) {
+	if (cdev_add(&my_device, my_device_nr, 1) == -1)
+	{
 		printk("Registering of device to kernel failed!\n");
 		goto AddError;
 	}
 
 	/* GPIO init */
-	if(gpio_request(LED_PIN, "gpio_leds_pin")) {
+	if (gpio_request(LED_PIN, "gpio_leds_pin"))
+	{
 		printk("Can not allocate GPIO led pin\n");
 		goto AddError;
 	}
 
 	/* Set GPIO direction */
-	if(gpio_direction_output(LED_PIN, 0)) {
+	if (gpio_direction_output(LED_PIN, 0))
+	{
 		printk("Can not set GPIO led pin to output!\n");
 		goto GpioError;
 	}
+
+	hrtimer_init(&hrtimer_one, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	hrtimer_one.function = &hrtimer_one_handler;
 
 	printk("Done\n");
 	return 0;
@@ -230,7 +292,8 @@ ClassError:
 	return -1;
 }
 
-static void __exit ModuleExit(void){
+static void __exit ModuleExit(void)
+{
 	gpio_set_value(LED_PIN, 0);
 	gpio_free(LED_PIN);
 	cdev_del(&my_device);
